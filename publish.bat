6044ddbbf50c7ffa5e23bc57a113be952761e820
Tomasz Fidos <tfidos@gmail.com>
2026-02-20 15:48:46 +0100

    publish

--- publish.bat
+++ publish.bat
@@ -1,2 +1,6 @@
-dotnet publish -c Release -r win-x64 --self-contained true /p:PublishSingleFile=true /p:EnableCompressionInSingleFile=true /p:DebugType=None /p:DebugSymbols=false  /p:PublishTrimmed=true /p:TrimMode=partial
+cd SadFontsUtil\SadFontsUtil\
+dotnet publish -c Release -r win-x64 --self-contained true /p:PublishSingleFile=true /p:EnableCompressionInSingleFile=true /p:DebugType=None /p:DebugSymbols=false  /p:PublishTrimmed=true /p:TrimMode=partial -o "../../Publish/CLI"
+cd ..\SadFontsUtilGUI
+dotnet publish -c Release -r win-x64 --self-contained true /p:PublishSingleFile=true /p:EnableCompressionInSingleFile=true /p:DebugType=None /p:DebugSymbols=false  -o "../../Publish/GUI"
+
 pause
